d983e9c33a8bd9334688872ee8f795fdc1c02690
Minh <nguyenngocminh190901@gmail.com>
2024-07-24 01:39:07 +0700

    update

--- src/modules/cats/cats.controller.ts
+++ src/modules/cats/cats.controller.ts
@@ -1,20 +1,61 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseFilters,
+  UsePipes,
+} from '@nestjs/common';
 
-import { CreateCatDto } from './dtos/create-cat.dto';
+import { CreateCatDto, createCatSchema } from './dtos/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { ForbiddenException } from 'src/exceptions/forbidden.exception';
+import { HttpExceptionFilter } from 'src/exceptions/http-exception.filter';
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(createCatSchema))
   create(@Body() createCatDto: CreateCatDto) {
-    this.catsService.create(createCatDto);
+    this.catsService.create(createCatDto as Cat);
   }
 
   @Get()
   async findAll(): Promise<Cat[]> {
     return this.catsService.findAll();
   }
+
+  @Get(':id')
+  async findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return this.catsService.findOne(id);
+  }
+
+  // @Post()
+  // @UseFilters(new HttpExceptionFilter())
+  // async create(@Body() createCatDto: CreateCatDto) {
+  //   throw new ForbiddenException();
+  // }
+
+  // @Get()
+  // async findAll() {
+  //   try {
+  //     await this.catsService.findAll();
+  //   } catch (error) {
+  //     throw new ForbiddenException();
+  //   }
+  // }
 }
--- src/modules/cats/cats.service.ts
+++ src/modules/cats/cats.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { Cat } from './interfaces/cat.interface';
+import { CreateCatDto } from './dtos/create-cat.dto';
 
 @Injectable()
 export class CatsService {
   private readonly cats: Cat[] = [];
 
   create(cat: Cat) {
-    this.cats.push(cat);
+    this.cats.push({ ...cat, id: this.cats.length + 1 });
   }
 
   findAll(): Cat[] {
     return this.cats;
+  }
+
+  findOne(id: number) {
+    return this.cats.find((cat) => cat?.id === id);
   }
 }
